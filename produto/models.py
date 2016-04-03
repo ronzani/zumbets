4cbb119e490d388c26d7010d49d0205cb57a4d1e
ronzani <ronzani.robson@gmail.com>
2016-04-02 22:16:02 -0300

    - Teste

--- produto/models.py
+++ produto/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 CHOICES_MODELO = (('b', 'Babylook'),
-                  ('n', 'Normal'),
+                  ('n', 'Norma'),
                   )
 
 
